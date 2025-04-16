5069689c5b39ea4247b2a0406e5b658d77598bb2
DeividGalindo <deivid.dd223@gmail.com>
2025-04-16 16:42:43 -0300

    Feito alterações no layout, reogarnizado as informações do nosso site, adicionavo novas funções, adicionado organizadores, adicionado um gif de fundo, feito melhorias na estilização.

---
+++ script.js
@@ -0,0 +1,19 @@
+function openPopup() {
+    document.getElementById('popup').classList.remove('hidden');
+}
+
+function closePopup() {
+    document.getElementById('popup').classList.add('hidden');
+}
+
+let index = 0;
+
+function moveCarousel(step) {
+    const track = document.querySelector('.carousel-track');
+    const width = 320;
+    index += step;
+    const max = track.children.length - 1;
+    if (index < 0) index = 0;
+    if (index > max) index = max;
+    track.scrollTo({ left: width * index, behavior: 'smooth' });
+}
